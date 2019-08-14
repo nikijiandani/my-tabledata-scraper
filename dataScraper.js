9e802bf9cf06cad2f07b742a1170048bf61de477
Nikita Jiandani <niki.jiandani@gmail.com>
2019-08-14 10:33:11 -0400

    Add prettier

--- dataScraper.js
+++ dataScraper.js
@@ -1,9 +1,9 @@
-const { csvFormat } = require('d3-dsv');
-const Nightmare = require('nightmare');
-const { writeFileSync } = require('fs');
+const { csvFormat } = require("d3-dsv");
+const Nightmare = require("nightmare");
+const { writeFileSync } = require("fs");
 
 const mySearch = "datatables";
-const START = 'https://www.google.com/';
+const START = "https://www.google.com/";
 
 const getData = async myInput => {
   console.log(`Searching Google for ${myInput}...`);
@@ -13,19 +13,17 @@ const getData = async myInput => {
   try {
     await nightmare
       .goto(START)
-      .wait('input[title=Search]')
-      .type('input[title=Search]', `${myInput}\u000d`);
-  } catch(e) {
+      .wait("input[title=Search]")
+      .type("input[title=Search]", `${myInput}\u000d`);
+  } catch (e) {
     console.error(e);
     return undefined;
   }
 
   //waits for the search results to load then clicks on the first result
   try {
-    await nightmare
-      .wait('.rc .r a')
-      .click('.rc .r a')
-  } catch(e) {
+    await nightmare.wait(".rc .r a").click(".rc .r a");
+  } catch (e) {
     console.error(e);
     return undefined;
   }
@@ -33,36 +31,37 @@ const getData = async myInput => {
   //waits for the example table to load, if the page title matches what we're looking for, it returns an array of objects.
   try {
     const table = await nightmare
-      .wait('#example')
+      .wait("#example")
       .evaluate(() => {
-        if(document.title === "DataTables | Table plug-in for jQuery"){
-          return [...document.querySelectorAll('#example')[0].children[1].children]
-            .map(el => {
-              return {
-                name: el.children[0].innerText,
-                position: el.children[1].innerText,
-                office: el.children[2].innerText,
-                age: el.children[3].innerText,
-                startDate: el.children[4].innerText
-              }
-            })
+        if (document.title === "DataTables | Table plug-in for jQuery") {
+          return [
+            ...document.querySelectorAll("#example")[0].children[1].children
+          ].map(el => {
+            return {
+              name: el.children[0].innerText,
+              position: el.children[1].innerText,
+              office: el.children[2].innerText,
+              age: el.children[3].innerText,
+              startDate: el.children[4].innerText
+            };
+          });
         }
       })
       .end();
 
-  return [...table];
-  } catch(e) {
+    return [...table];
+  } catch (e) {
     console.error(e);
     return undefined;
   }
-}
+};
 
 //fetches data, filters it for any undefined values, reformats it to a CSV string and creates an output dataset
 getData(mySearch)
   .then(data => {
     const csvData = csvFormat(data.filter(i => i));
-    console.log('Exporting results to output.csv...')
-    writeFileSync('./output.csv', csvData, { encoding: 'utf8' });
-    console.log('Done!')
+    console.log("Exporting results to output.csv...");
+    writeFileSync("./output.csv", csvData, { encoding: "utf8" });
+    console.log("Done!");
   })
   .catch(e => console.error(e));
