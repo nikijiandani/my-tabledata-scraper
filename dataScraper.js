85e4fec07accf06fde6a53dcac9c5bf2514a0727
Nikita Jiandani <niki.jiandani@gmail.com>
2019-08-14 10:22:41 -0400

    Add comments, logs, conditional title match

--- dataScraper.js
+++ dataScraper.js
@@ -3,14 +3,13 @@ const Nightmare = require('nightmare');
 const { writeFileSync } = require('fs');
 
 const mySearch = "datatables";
-
 const START = 'https://www.google.com/';
 
 const getData = async myInput => {
   console.log(`Searching Google for ${myInput}...`);
   const nightmare = new Nightmare({ show: true });
 
-  //Go to search page
+  //navigates to URL, waits for the Search input to load and searches for given value
   try {
     await nightmare
       .goto(START)
@@ -18,9 +17,10 @@ const getData = async myInput => {
       .type('input[title=Search]', `${myInput}\u000d`);
   } catch(e) {
     console.error(e);
+    return undefined;
   }
-  
-  //click on the first search result link
+
+  //waits for the search results to load then clicks on the first result
   try {
     await nightmare
       .wait('.rc .r a')
@@ -30,24 +30,26 @@ const getData = async myInput => {
     return undefined;
   }
 
+  //waits for the example table to load, if the page title matches what we're looking for, it returns an array of objects.
   try {
     const table = await nightmare
       .wait('#example')
       .evaluate(() => {
-        return [...document.querySelectorAll('#example')[0].children[1].children]
-          .map(el => {
-            return {
-              name: el.children[0].innerText,
-              position: el.children[1].innerText,
-              office: el.children[2].innerText,
-              age: el.children[3].innerText,
-              startDate: el.children[4].innerText
-            }
-          })
+        if(document.title === "DataTables | Table plug-in for jQuery"){
+          return [...document.querySelectorAll('#example')[0].children[1].children]
+            .map(el => {
+              return {
+                name: el.children[0].innerText,
+                position: el.children[1].innerText,
+                office: el.children[2].innerText,
+                age: el.children[3].innerText,
+                startDate: el.children[4].innerText
+              }
+            })
+        }
       })
       .end();
-  
-  console.log(table);
+
   return [...table];
   } catch(e) {
     console.error(e);
@@ -55,9 +57,12 @@ const getData = async myInput => {
   }
 }
 
+//fetches data, filters it for any undefined values, reformats it to a CSV string and creates an output dataset
 getData(mySearch)
   .then(data => {
     const csvData = csvFormat(data.filter(i => i));
+    console.log('Exporting results to output.csv...')
     writeFileSync('./output.csv', csvData, { encoding: 'utf8' });
+    console.log('Done!')
   })
   .catch(e => console.error(e));
